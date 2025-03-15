ebfdb82f9e4200ab5b9f5e3cb9a2dd7c245aa11a
dbstjd7084 <gmdqn96@gmail.com>
2025-03-15 11:28:03 +0900

    [Silver V] Title: 분수찾기, Time: 36 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -2,19 +2,14 @@ input = int(input())
 if (input == 1):
     print ("1/1")
 else:
-    count = 1
+    count = 2
     while input > count*(count+1)/2:
         count+=1
-    합 = count*(count+1)/2
-    diff = 합 - input
-    분모 = 1
-    분자 = count
-
+    diff = count*(count+1)/2 - input
     if (count % 2 == 1):
         분모 = count - diff
         분자 = 1 + diff
     else:
         분자 = count - diff
         분모 = 1 + diff
-
     print(f"{int(분자)}/{int(분모)}")
