85dd08b56020f46a3338bb940f954fb4125bc43d
dbstjd7084 <gmdqn96@gmail.com>
2025-03-17 00:50:37 +0900

    [Silver IV] Title: 제로, Time: 76 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,9 +1,16 @@
-count = int(input())
-stack = []
+import sys
+# 테스트 실행 횟수 입력받기
+count = int(sys.stdin.readline())
+total = 0
+before = []
+# 실행 횟수만큼 입력받기
 for __ in range(count):
-    inp = int(input())
+    inp = int(sys.stdin.readline())
+    # 0이면 pop
     if (inp == 0):
-        stack.pop()
+        total-=before.pop()
+    # 아니면 push 및 합 계산
     else:
-        stack.append(inp)
-print(sum(stack))
+        before.append(inp)
+        total+=inp
+print(total)
