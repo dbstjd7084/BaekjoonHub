5121b34a3eeeb08ee2f302009cb677908a3a1e8c
dbstjd7084 <gmdqn96@gmail.com>
2025-03-21 22:56:06 +0900

    [Bronze V] Title: 몇개고?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27294. 몇개고？/몇개고？.py
+++ 백준/Bronze/27294. 몇개고？/몇개고？.py
@@ -4,7 +4,7 @@ import sys
 T, S = tuple(map(int, sys.stdin.readline().split()))
 
 # 점심시간이고 술이 없으면 320
-if T >= 12 and T <= 16 and S == 0:
+if S == 0 and T >= 12 and T <= 16:
     print(320)
 else:
     print(280)
