2a1d8e376175e7b37b4d0ceccf3f4df58b1a1798
dbstjd7084 <gmdqn96@gmail.com>
2025-03-30 23:25:33 +0900

    [Silver V] Title: 숫자 카드, Time: 1972 ms, Memory: 111116 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,25 @@
+def binary_search(arr, target):
+    start = 0
+    end = N - 1 # end = len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return 1
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return 0
+
+
+import sys
+
+N = int(sys.stdin.readline())
+cards = list(map(int, sys.stdin.readline().split()))
+cards.sort()
+
+M = int(sys.stdin.readline())
+check = list(map(int, sys.stdin.readline().split()))
+
+for target in check:
+    print(binary_search(cards, target))
