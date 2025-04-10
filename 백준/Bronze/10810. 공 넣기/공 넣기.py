a88506e1ef4045a5200bd62d52615475760d8462
dbstjd7084 <gmdqn96@gmail.com>
2025-04-10 23:52:22 +0900

    [Bronze III] Title: 공 넣기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -6,8 +6,7 @@ basket = [0] * N
 for _ in range(M):
     i, j, k = map(int, sys.stdin.readline().split())
 
-    for change in range(i, j+1):
-        basket[change-1] = k
+    basket[i-1:j] = [k] * (j-i+1)
 
 # 출력력
 print(*basket)
