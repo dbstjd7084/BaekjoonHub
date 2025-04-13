6e7672eb17dbccaa60f6addae2da083a6b8fb6d0
dbstjd7084 <gmdqn96@gmail.com>
2025-04-14 01:39:48 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,20 @@
 import sys
+input = sys.stdin.readline
 
-N, M = map(int, sys.stdin.readline().split())
-visited = [False] * (N)
+N, M = map(int, input().split())
+visited = [False] * (N + 1)
 path = []
 
-def 백트래킹():
-    # path의 수열 개수가 M개일 때 출력
+def backtrack():
     if len(path) == M:
         print(*path)
         return
-    # 아니라면 포함이 되어있지 않는 요소 추가해 재귀
-    for i in range(1, N+1):
-        if i not in path:
-            visited[i-1] = True
+    for i in range(1, N + 1):
+        if not visited[i]:
+            visited[i] = True
             path.append(i)
-            백트래킹()
+            backtrack()
             path.pop()
-            visited[i-1] = False
+            visited[i] = False
 
-백트래킹()
+backtrack()
