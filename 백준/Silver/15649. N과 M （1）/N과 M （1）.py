e434eb7c30630a8587c64662ce2b99fa6b6a0119
dbstjd7084 <gmdqn96@gmail.com>
2025-04-14 01:29:00 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,10 +1,10 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
+visited = [False] * (N)
+path = []
 
-M개_수열 = [i+1 for i in range(M)]
-
-def 백트래킹(path):
+def 백트래킹():
     # path의 수열 개수가 M개일 때 출력
     if len(path) == M:
         print(*path)
@@ -12,6 +12,10 @@ def 백트래킹(path):
     # 아니라면 포함이 되어있지 않는 요소 추가해 재귀
     for i in range(1, N+1):
         if i not in path:
-            백트래킹(path + [i])
+            visited[i-1] = True
+            path.append(i)
+            백트래킹()
+            path.pop()
+            visited[i-1] = False
 
-백트래킹([])
+백트래킹()
