1946c743575e9e6549596b0827d7422c2993fde4
dbstjd7084 <gmdqn96@gmail.com>
2025-08-07 04:23:24 +0900

    [Gold IV] Title: 부분합, Time: 88 ms, Memory: 44904 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,27 +1,35 @@
-import sys
+from sys import stdin
 
-N, S = map(int, sys.stdin.readline().split())
-seq = list(map(int, sys.stdin.readline().split()))
+N, S = map(int, stdin.readline().split())
+seq = list(map(int, stdin.readline().split()))
 
-end = 0
-# 구하려는 길이 변수
-length = 0
 부분합 = 0
-
-# 반복문 시작
-for start in range(N):
-    while 부분합 < S and end < N:
-        # 부분합 구하기
-        부분합 += seq[end]
-        end += 1
-    # S보다 크거나 같으면
+start = 0
+# S 이상의 부분합이 될 때까지 end 포인터 증가
+for end_idx, end_value in enumerate(seq, 1):
+    부분합 += end_value
+    # 찾으면 탈출
     if 부분합 >= S:
-        # length에 길이 저장
-        # 만약 length에 이미 작은 값이 있다면 스킵
-        if length > end - start or length == 0:
-            length = end - start
-    
+        break
+else:
+    # 없으면 0 출력 및 중단
+    print(0)
+    exit()
+
+# 부분합이 S 미만 될 때까지 start 포인터를 계속 증가시킴
+while True:
     부분합 -= seq[start]
+    start += 1
+    # 만약 부분합이 S보다 작으면
+    if 부분합 < S:
+        # end 포인터의 idx(인덱스)가 끝에 도달한 경우 undo 후 탈출
+        if end_idx == N:
+            start -= 1
+            break
+        # end 포인터의 idx(인덱스)가 끝이 아닌 경우
+        # end 포인터를 증가시킴
+        부분합 += seq[end_idx]
+        end_idx += 1
 
 # 출력
-print(length)
+print(end_idx - start)
